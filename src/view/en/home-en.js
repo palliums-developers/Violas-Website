18e4241a159eb92290620fefa2f3096e1333ff2e
waitingmh <15210040433@163.com>
2020-02-18 10:57:07 +0800

    home

--- src/view/en/home-en.js
+++ src/view/en/home-en.js
@@ -4,8 +4,12 @@ class HomeEN extends Component{
   render(){
     return (
         <div className="home">
-           <div className="headtalk">
-             
+           <div className="headContent">
+             <div className="inset"><img src="/img/矩形 6@2x.png"/></div>
+             <div className="headTalk">
+                <p>Welcome to Violas</p>
+                <p><label for="">WhitePaper</label><img src="/img/编组 45复制 3@2x.png" /></p>
+             </div>
            </div>
         </div>
     );
