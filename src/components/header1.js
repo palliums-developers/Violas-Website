8a353620c0cf9e2313d65f869704cf9dd05f679b
nissaka <maigechuang115@163.com>
2020-02-21 18:36:17 +0800

    fix whitepaper bugs

--- src/components/header1.js
+++ src/components/header1.js
@@ -7,30 +7,30 @@ class Header1 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clWidth:null,
+      clWidth: null,
       open: false,
     }
   }
   componentWillMount() {
     intl.options.currentLocale = localStorage.getItem("local");
     this.setState({
-      clWidth:document.body.clientWidth
+      clWidth: document.body.clientWidth
     })
   }
   onOpenChange = (...args) => {
-    this.setState({ open: true },()=>{
+    this.setState({ open: true }, () => {
       console.log(this.state.open)
       this.props.getChange(this.state.open)
     });
   }
   goEN() {
-    return <a onClick={()=>{localStorage.setItem('local','EN')}}>{intl.get('header.en')}</a>
+    return <a onClick={() => { localStorage.setItem('local', 'EN'); this.forceUpdate(); }}>{intl.get('header.en')}</a>
   }
   goKO() {
-    return <a onClick={()=>{localStorage.setItem('local','KO')}}>{intl.get('header.ko')}</a>
+    return <a onClick={() => { localStorage.setItem('local', 'KO'); this.forceUpdate(); }}>{intl.get('header.ko')}</a>
   }
   goJA() {
-    return <a onClick={()=>{localStorage.setItem('local','JA')}}>{intl.get('header.ja')}</a>
+    return <a onClick={() => { localStorage.setItem('local', 'JA'); this.forceUpdate(); }}>{intl.get('header.ja')}</a>
   }
   twoLanguages(lang) {
     switch (lang) {
@@ -42,41 +42,41 @@ class Header1 extends Component {
         return <>{this.goJA()}{this.goEN()}</>
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     // this.setState({
     //   open:this.props.opens
     // })
-    window.addEventListener('resize', (e)=>{
+    window.addEventListener('resize', (e) => {
       this.setState({
-        clWidth:e.target.innerWidth
+        clWidth: e.target.innerWidth
       })
     });
   }
   render() {
+    intl.options.currentLocale = localStorage.getItem('local');
     return (
       this.state.clWidth > 1024 ? <header className="header">
-      <div className="head">
-        <div className="logo">
-          <a href="index.html"><img src="/img/编组 252@2x.png" /></a>
+        <div className="head">
+          <div className="logo">
+            <a href="index.html"><img src="/img/编组 252@2x.png" /></a>
+          </div>
+          <ul className="navList">
+            <li id="vision" onClick={() => { this.props.history.push('/app/vision') }}>{intl.get('header.vision')}</li>
+            <li id="association" onClick={() => { this.props.history.push('/app/association') }}>{intl.get('header.association')}</li>
+            <li id="partners" onClick={() => { this.props.history.push('/app/partners') }}>{intl.get('header.partners')}</li>
+            <li>{intl.get('header.blockchain explorer')}</li>
+            <li id="media" onClick={() => { this.props.history.push('/app/media') }}>{intl.get('header.media')}</li>
+            <li id="developers" onClick={() => { this.props.history.push('/developers') }}>{intl.get('header.developers')}</li>
+          </ul>
+          <div className="descr">
+            <span id="whitePaper" onClick={() => {
+              this.props.history.push('/whiteParper')
+            }}>{intl.get('header.white paper')}</span>
+            {this.twoLanguages(localStorage.getItem("local"))}
+          </div>
         </div>
-        <ul className="navList">
-          <li id="vision" onClick={()=>{this.props.history.push('/app/vision')}}>{intl.get('header.vision')}</li>
-          <li id="association" onClick={()=>{this.props.history.push('/app/association')}}>{intl.get('header.association')}</li>
-          <li id="partners" onClick={()=>{this.props.history.push('/app/partners')}}>{intl.get('header.partners')}</li>
-          <li>{intl.get('header.blockchain explorer')}</li>
-          <li id="media" onClick={()=>{this.props.history.push('/app/media')}}>{intl.get('header.media')}</li>
-          <li id="developers" onClick={()=>{this.props.history.push('/developers')}}>{intl.get('header.developers')}</li>
-        </ul>
-        <div className="descr">
-          <span id="whitePaper" onClick={()=>{
-            this.props.history.push('/whiteParper')
-          }}>White Paper</span>
-          <a>日本語</a>
-          <a>한국어</a>
-        </div>
-        </div>
-    </header> : <NavBar leftContent={<img onClick={()=>{this.props.history.push('/app')}} src="/img/编组 74复制 3@2x.png"/>} rightContent={<img src="/img/phone_nav.png" onClick={this.onOpenChange} />}>
-    </NavBar>
+      </header> : <NavBar leftContent={<img onClick={() => { this.props.history.push('/app') }} src="/img/编组 74复制 3@2x.png" />} rightContent={<img src="/img/phone_nav.png" onClick={this.onOpenChange} />}>
+        </NavBar>
     );
   }
 }
--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -5,52 +5,60 @@ import { List } from 'antd-mobile';
 import { withRouter } from 'react-router-dom';
 
 class SideBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          open: false,
-        }
-      }  
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    }
+  }
   componentWillMount() {
     intl.options.currentLocale = localStorage.getItem("local");
+  }
+  goEN() {
+    return <a onClick={() => { localStorage.setItem('local', 'EN'); this.forceUpdate(); }}>{intl.get('header.en')}</a>
+  }
+  goKO() {
+    return <a onClick={() => { localStorage.setItem('local', 'KO'); this.forceUpdate(); }}>{intl.get('header.ko')}</a>
+  }
+  goJA() {
+    return <a onClick={() => { localStorage.setItem('local', 'JA'); this.forceUpdate(); }}>{intl.get('header.ja')}</a>
   }
   twoLanguages(lang) {
     switch (lang) {
       case 'EN':
-        return <><span>{intl.get('header.ja')}</span><span>{intl.get('header.ko')}</span></>
+        return <>{this.goJA()}{this.goKO()}</>
       case 'JA':
-        return <><span>{intl.get('header.en')}</span><span>{intl.get('header.ko')}</span></>
+        return <>{this.goEN()}{this.goKO()}</>
       case 'KO':
-        return <><span>{intl.get('header.ja')}</span><span>{intl.get('header.en')}</span></>
+        return <>{this.goJA()}{this.goEN()}</>
     }
   }
   onOpenChange = (...args) => {
-    this.setState({ open: false },()=>{
-        this.props.getChange(this.state.open)
+    this.setState({ open: false }, () => {
+      this.props.getChange(this.state.open)
     });
   }
   render() {
+    intl.options.currentLocale = localStorage.getItem('local');
     return (
-        <div id="dw-p1" class="bmd-layout-drawer bg-faded">
-         <a class="home" href="index.html"><img src="../img/home 2.png" /></a>
-         <span class="close" onClick={()=>this.onOpenChange()}><img src="../img/guanbi-2备份 2.png" /></span>
-         <ul class="list-group phone_list">
-           <a onClick={() => { this.props.history.push('/app/vision') }}>Vision</a>
-           <a onClick={() => { this.props.history.push('/app/association') }}>Association</a>
-           <a onClick={() => { this.props.history.push('/app/partners') }}>Partners</a>
-           <a>Blockchain Explorer</a>
-           <a onClick={()=>{this.props.history.push('/app/media')}}>Media</a>
-           <a onClick={()=>{this.props.history.push('/developers')}}>Developers</a>
-           <span id="whitePaper1" onClick={() => {
-             this.props.history.push('/whiteParper')
-           }}>White Paper</span>
-           <div class="change">
-             {/* <!-- <a>EN</a> --> */}
-             <a href="../ja/index.html">日本語</a>
-             <a href="../ko/index.html" class="ka">한국어</a>
-           </div>
-         </ul>
-       </div>
+      <div id="dw-p1" class="bmd-layout-drawer bg-faded">
+        <a class="home" href="index.html"><img src="../img/home 2.png" /></a>
+        <span class="close" onClick={() => this.onOpenChange()}><img src="../img/guanbi-2备份 2.png" /></span>
+        <ul class="list-group phone_list">
+          <a onClick={() => { this.props.history.push('/app/vision') }}>{intl.get('header.vision')}</a>
+          <a onClick={() => { this.props.history.push('/app/association') }}>{intl.get('header.association')}</a>
+          <a onClick={() => { this.props.history.push('/app/partners') }}>{intl.get('header.partners')}</a>
+          <a>{intl.get('header.blockchain explorer')}</a>
+          <a onClick={() => { this.props.history.push('/app/media') }}>{intl.get('header.media')}</a>
+          <a onClick={() => { this.props.history.push('/developers') }}>{intl.get('header.developers')}</a>
+          <span id="whitePaper1" onClick={() => {
+            this.props.history.push('/whiteParper')
+          }}>{intl.get('header.white paper')}</span>
+          <div class="change">
+          {this.twoLanguages(localStorage.getItem("local"))}
+          </div>
+        </ul>
+      </div>
     );
   }
 }
