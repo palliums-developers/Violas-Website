88c36b7ccd2f1e991d892d81777945ce06d52417
nissaka <maigechuang115@163.com>
2020-03-02 18:10:20 +0800

    change ico and build

---
+++ build/precache-manifest.678fc81b6b375a65f46cfc191f3f8f69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0efe456fdf24b241558e19ad5d8a842",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d4dc7163c078149c181",
+    "url": "./static/css/2.7dc7ac9c.chunk.css"
+  },
+  {
+    "revision": "4f545f606b30b6237962",
+    "url": "./static/css/main.1112d42d.chunk.css"
+  },
+  {
+    "revision": "0d4dc7163c078149c181",
+    "url": "./static/js/2.90fbaeeb.chunk.js"
+  },
+  {
+    "revision": "50ecf82c32a23c3498738aa326c7cd79",
+    "url": "./static/js/2.90fbaeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f545f606b30b6237962",
+    "url": "./static/js/main.14819512.chunk.js"
+  },
+  {
+    "revision": "27bfb7d147e222246205",
+    "url": "./static/js/runtime-main.71501369.js"
+  },
+  {
+    "revision": "805a1e6680520eea22539e9b6b2ad9e2",
+    "url": "./static/media/Open-Sans-2.805a1e66.ttf"
+  }
+]);
