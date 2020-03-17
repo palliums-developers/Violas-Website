5a67a22ee235523f84b0f10e8a37ddbdef08caf9
nissaka <maigechuang115@163.com>
2020-03-18 07:30:14 +0800

    using wordpress

--- violas.io/gatsby-config.js
+++ violas.io/gatsby-config.js
@@ -27,6 +27,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `http://47.52.66.26:10087`,
+        protocol: `http`,
+        hostingWPCOM: false,
+        useACF: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
