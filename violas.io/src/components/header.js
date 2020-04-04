cd8967e6c0f0af0bb1f00fff9076d2a4ebbf5d56
nissaka <maigechuang115@163.com>
2020-04-05 01:59:10 +0800

    fix header css

--- violas.io/src/components/header.js
+++ violas.io/src/components/header.js
@@ -4,6 +4,7 @@ import { graphql, Link, useStaticQuery, StaticQuery } from "gatsby"
 import React, { Component } from "react"
 import langLink from "./langLink"
 import Img from "gatsby-image"
+import "../style/header.css"
 
 import logoWhite_jpg from "../images/logo-white.png"
 import logoPurple_jpg from "../images/logo-purple.png"
@@ -152,23 +153,33 @@ class Header extends Component {
     switch (_pages) {
       case "homepage":
         // return <img src="/static/9bb038b9ff4d9f1cede0b1232abd6e6c/14b42/homepage.jpg" />
-        return <img src={homepage_jpg} />
+        // return <img src={homepage_jpg} />
+        return homepage_jpg
       case "vision":
         // return <img src="/static/3cad50fb229e266066a9bdf3996fd227/14b42/vision.jpg" />
-        return <img src={vision_jpg} />
+        // return <img src={vision_jpg} />
+        return vision_jpg
       case "association":
         // return <img src="/static/b9880b6f7ff04cd9bb3e35c078570490/14b42/association.jpg" />
-        return <img src={association_jpg} />
+        // return <img src={association_jpg} />
+        return association_jpg
       case "partners":
         // return <img src="/static/82dd86db42801d541b03592db593786b/14b42/partners.jpg" />
-        return <img src={partners_jpg} />
+        // return <img src={partners_jpg} />
+        return partners_jpg
       case "media":
         // return <img src="/static/2937f6503b6e9bf3b89b6e28fabc842a/14b42/media.jpg" />
         // return <Img fluid={data.media.childImageSharp.fluid}/>
-        return <img src={media_jpg} />
-      case "logo":
+        // return <img src={media_jpg} />
+        return media_jpg
+      case "logo-purple":
         // return <img src="/static/c7a541a81abe46e6e59775bf7782f50d/75ec2/logo-purple.png" />
-        return <img src={logoPurple_jpg} />
+        // return <img src={logoPurple_jpg} />
+        return logoPurple_jpg
+      case "logo-white":
+        return logoWhite_jpg
+      default:
+        return logoWhite_jpg;
     }
 
     // let imgData = await graphql(`
@@ -202,18 +213,23 @@ class Header extends Component {
     return (
       <header
         style={{
-          // background: `rebeccapurple`,
-          background: `black`,
+          background: `transparent`,
+          // backgroundImage: url(this.headerImg(this.props.wp_path.split("-")[0])),
           // height:`720px`
           // marginBottom: `1.45rem`,
-        }}
+          position: 'absolute',
+          top: '0px'
+        }
+        }
       >
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            height: `100px`
+            // margin: `0 auto`,
+            // maxWidth: 1200,
+            // height: `100px`,
             // padding: `1.45rem 1.0875rem`,
+            // background:"black"
+            // backgroundImage: 'url(' + this.headerImg(this.props.wp_path.split("-")[0] ? this.props.wp_path.split("-")[0] : '111') + ')',
           }}
           className="head"
         >
@@ -221,10 +237,12 @@ class Header extends Component {
             className="nav"
             dangerouslySetInnerHTML={{ __html: langLink(this.state.language) }}
           ></div> */}
+          <div className="linkPage">
             {
               // console.log(langLink(this.state.language))
               langLink(this.state.language)
             }
+          </div>
           {/* <div className="nav" >{langLink(this.state.language).map((item, index) => {
             return <>{item}</>
           })}</div> */}
@@ -237,12 +255,10 @@ class Header extends Component {
             <Link to="homepage-ja" onClick={this.storeSession.bind(this, "ja")}>日本語</Link>
             <Link to="homepage-ko" onClick={this.storeSession.bind(this, "ko")}>한국어</Link>
             {
-              this.headerImg(this.props.wp_path.split("-")[0])
-
+              // <img src={this.headerImg(this.props.wp_path.split("-")[0])}/>
               // this.headerImg.bind(this,this.props.wp_path.split("-")[0])
             }
-            }
-        </div>
+          </div>
         </div>
         {
           // this.props.language==="ja"?<>{this.clickLang("ja")}</>:this.props.language==="ko"?<>{this.clickLang("ko")}</>:<>{this.clickLang("en")}</>
@@ -252,7 +268,7 @@ class Header extends Component {
           <a aria-current="page" href={this.changeLang("ja")} onClick={this.changeSession("ja")}>日本語</a> */}
           </>
         }
-      </header>
+      </header >
     )
   }
 }
--- violas.io/src/templates/page.js
+++ violas.io/src/templates/page.js
@@ -4,11 +4,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Swiper from "swiper/js/swiper.js";
 import "swiper/css/swiper.min.css";
+
+import "../style/headContent.css"
+import logoWhite_jpg from "../images/logo-white.png"
+import logoPurple_jpg from "../images/logo-purple.png"
+import homepage_jpg from "../images/homepage.jpg"
+import vision_jpg from "../images/vision.jpg"
+import association_jpg from "../images/association.jpg"
+import partners_jpg from "../images/partners.jpg"
+import media_jpg from "../images/media.jpg"
+import cover_png from "../images/cover.png"
+
 class Page extends Component {
   componentWillMount() {
     this.followLang()
   }
-  componentDidMount(){
+  componentDidMount() {
     var mySwiper = new Swiper(".swiper-container1", {
       slidesPerView: 3.3,
       spaceBetween: 20,
@@ -30,9 +41,9 @@ class Page extends Component {
       window.location = slug.split('-')[0] + '-' + temp_lang;
     }
   }
-  langLimit(){
-    let _temp=JSON.parse(sessionStorage.getItem("violas-lang"));
-    switch (_temp){
+  langLimit() {
+    let _temp = JSON.parse(sessionStorage.getItem("violas-lang"));
+    switch (_temp) {
       case "ko":
         return "ko";
       case "ja":
@@ -41,15 +52,53 @@ class Page extends Component {
         return "en";
     }
   }
+  selectBackgroundImg(_pageName) {
+    switch (_pageName) {
+      case "homepage":
+        // return <img src="/static/9bb038b9ff4d9f1cede0b1232abd6e6c/14b42/homepage.jpg" />
+        // return <img src={homepage_jpg} />
+        return homepage_jpg
+      case "vision":
+        // return <img src="/static/3cad50fb229e266066a9bdf3996fd227/14b42/vision.jpg" />
+        // return <img src={vision_jpg} />
+        return vision_jpg
+      case "association":
+        // return <img src="/static/b9880b6f7ff04cd9bb3e35c078570490/14b42/association.jpg" />
+        // return <img src={association_jpg} />
+        return association_jpg
+      case "partners":
+        // return <img src="/static/82dd86db42801d541b03592db593786b/14b42/partners.jpg" />
+        // return <img src={partners_jpg} />
+        return partners_jpg
+      case "media":
+        // return <img src="/static/2937f6503b6e9bf3b89b6e28fabc842a/14b42/media.jpg" />
+        // return <Img fluid={data.media.childImageSharp.fluid}/>
+        // return <img src={media_jpg} />
+        return media_jpg
+      case "logo-purple":
+        // return <img src="/static/c7a541a81abe46e6e59775bf7782f50d/75ec2/logo-purple.png" />
+        // return <img src={logoPurple_jpg} />
+        return logoPurple_jpg
+      case "logo-white":
+        return logoWhite_jpg
+      default:
+        return logoWhite_jpg;
+    }
+  }
   render() {
-    const StaticPage = this.props.data.wordpressPage
+    const StaticPage = this.props.data.wordpressPage;
+    // backgroundImage: 'url(' + this.headerImg(this.props.wp_path.split("-")[0] ? this.props.wp_path.split("-")[0] : '111') + ')',
+    // console.log(this.props.path.split("/")[1].split("-")[0]);
     return (
       <>
-      {/* {
+        {/* {
         this.props.data.wordpressPage.slug.toString()==="violas-blog"?
 
       } */}
-          <SEO title="violas pages" />
+        <SEO title="violas pages" />
+        <div className="headContent" style={{
+          backgroundImage: 'url(' + this.selectBackgroundImg(this.props.path.split("/")[1].split("-")[0]) + ')' + ', url(' + cover_png + ')',
+        }}></div>
         <Layout>
           <div dangerouslySetInnerHTML={{ __html: StaticPage.content }}></div>
         </Layout>
